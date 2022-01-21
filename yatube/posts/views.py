526641e07465f4e2089bc3c080dda5411ada7d3b
unknown <C:\Users\pkgodovikov\AppData\Roaming\The Bat!>
2022-01-21 19:41:03 +0300

    Link in index.html doesnt working row 24

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,20 +1,28 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
 
 
 def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
     template = 'posts/index.html'
     text = 'Это главная страница проекта Yatube'
     context = {
-        'text' : text
+        'posts': posts,
+        'text': text,
     }
     return render(request, template, context)
 
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    template = 'posts/group_list.html/'
     text = 'Здесь будет информация о группах проекта Yatube'
     context = {
-        'text': text
+        'group': group,
+        'posts': posts,
+        'text': text,
     }
     return render(request, template, context)
